4e1e7263d6d89c0cd249b17f3a8cf4a982d17743
HellenNzisa <hellen.mutemi@strathmore.edu>
2025-09-01 10:31:15 +0300

    error handling

--- layouts/layouts.php
+++ layouts/layouts.php
@@ -1,13 +1,10 @@
 <?php
-
-
-class Layout {
-    public function header($config) {
-        return "<header><h1>Welcome to " . $config['site_title'] . "</h1></header>";
+class layouts {
+    public function header($conf) {
+        echo "<header><h1>" . $conf['site_name'] . "</h1></header>";
     }
 
-    public function footer($config) {
-        return "<footer><p>&copy; 2024 " . $config['site_title'] . "</p></footer>";
+    public function footer($conf) {
+        echo "<footer><p>Copyright &copy; - " . $conf['site_name'] . " " . date("Y") . "</p></footer>";
     }
 }
-?>
